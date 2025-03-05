012e44a54b59f5198b366e3a4020f9a68f79e51b
5237-mests <msfnmulgeta@gmail.com>
2025-03-05 11:17:38 +0300

    Initial commit.

---
+++ Python/file.py
@@ -0,0 +1,20 @@
+# open and read file
+# file = open("python.txt", "r")
+# content = file.read()
+# print(content)
+# file.close()
+
+# created_file = open("geeksforgeeks7.txt","x")
+
+# Check the file
+# print(open("geeksforgeeks.txt","r").read() == False)
+
+# Create a file and write content to it
+# with open("geeksforgeeks.txt", "w") as file:
+#     file.write("Hello, this is a sample file.\n")
+#     file.write("Python is awesome!")
+
+# Read the entire file content
+# with open("geeksforgeeks.txt", "r") as file:
+#     content = file.read()
+#     print(content)
